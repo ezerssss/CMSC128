79c8c9dfd551ab5047f6f4e51a223cc3921b61ec
ezerssss <magbanuaezra@gmail.com>
2024-11-11 23:26:09 +0800

    bug fix

--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -7,7 +7,7 @@ import { TrackingStatusEnum } from "@/app/enums/tracking";
 import { getTrackingStatusMessage } from "@/lib/tracking";
 import { doc, onSnapshot } from "firebase/firestore";
 import clientDb from "@/app/firebase/clientDB";
-
+import { useSearchParams } from "next/navigation";
 
 const icons = {
   [TrackingStatusEnum.CONFIRMED_ORDER]: <ShoppingCart size={43} />,
@@ -17,11 +17,13 @@ const icons = {
 };
 
 export default function Tracker() {
-  const query = new URLSearchParams(window.location.search);
-  const orderID = query.get('orderID');
-  const shopID = query.get('shopID');
-  
-  const [currentStatus, setCurrentStatus] = useState<TrackingStatusEnum>();
+  const searchParams = useSearchParams();
+  const orderID = searchParams.get("orderID");
+  const shopID = searchParams.get("shopID");
+
+  const [currentStatus, setCurrentStatus] = useState<TrackingStatusEnum>(
+    TrackingStatusEnum.CONFIRMED_ORDER
+  );
   const [complete, setComplete] = useState(false);
   const [orderHistory, setOrderHistory] = useState<any[]>([]);
   const statusArray = Object.values(TrackingStatusEnum);
@@ -58,26 +60,32 @@ export default function Tracker() {
   }, [orderID, shopID]);
 
   return (
-    <div className="flex flex-col items-center h-screen bg-gradient-to-br from-[#A6D3EF] to-[#EFF5FF]">
-      <div className="flex gap-[500px] fixed left-[80px] mt-[30px]">
+    <div className="flex h-screen flex-col items-center bg-gradient-to-br from-[#A6D3EF] to-[#EFF5FF]">
+      <div className="fixed left-[80px] mt-[30px] flex gap-[500px]">
         <div>
-          <p className="font-bold text-center text-[50px] text-[#173563] mb-5">Tracking Order</p>
-          <p className="font-normal bg-white text-[20px] text-center text-[#173563] p-[10px] rounded-[10px] shadow-2xl">
+          <p className="mb-5 text-center text-[50px] font-bold text-[#173563]">
+            Tracking Order
+          </p>
+          <p className="rounded-[10px] bg-white p-[10px] text-center text-[20px] font-normal text-[#173563] shadow-2xl">
             <strong>{orderID}</strong>
           </p>
         </div>
 
-        <div className="text-left overflow-y-auto mt-[30px]">
+        <div className="mt-[30px] overflow-y-auto text-left">
           {orderHistory.length > 0 ? (
             orderHistory.map((order, index) => (
               <div key={index} className="relative flex">
-                <div className={`bullet ${statusArray.indexOf(order.status) <= statusArray.indexOf(currentStatus) ? "active" : ""}`}></div>
-                {index < orderHistory.length - 1 && <div className="line"></div>}
+                <div
+                  className={`bullet ${statusArray.indexOf(order.status) <= statusArray.indexOf(currentStatus) ? "active" : ""}`}
+                ></div>
+                {index < orderHistory.length - 1 && (
+                  <div className="line"></div>
+                )}
                 <div>
-                  <p className="font-normal text-[12.5px] text-[#173563]">
+                  <p className="text-[12.5px] font-normal text-[#173563]">
                     <strong>{order.status}</strong> - {order.message}
                   </p>
-                  <p className="font-normal text-[12.5px] text-[#173563] mb-5">
+                  <p className="mb-5 text-[12.5px] font-normal text-[#173563]">
                     {order.date} | {order.time}
                   </p>
                 </div>
@@ -89,16 +97,23 @@ export default function Tracker() {
         </div>
       </div>
 
-      <div className="fixed bottom-[290px] flex justify-between mt-[20px]">
+      <div className="fixed bottom-[290px] mt-[20px] flex justify-between">
         {statusArray.map((status, index) => (
-          <div key={index} className={`status-line ${currentStatus === status ? "active" : ""} ${statusArray.indexOf(status) < statusArray.indexOf(currentStatus) || complete ? "complete" : ""}`}>
+          <div
+            key={index}
+            className={`status-line ${currentStatus === status ? "active" : ""} ${statusArray.indexOf(status) < statusArray.indexOf(currentStatus) || complete ? "complete" : ""}`}
+          >
             <div className="status">{icons[status]}</div>
-            <p className="font-bold text-[13px] text-[#173563]">{status}</p>
+            <p className="text-[13px] font-bold text-[#173563]">{status}</p>
           </div>
         ))}
       </div>
 
-      <img src="/images/tracker/basket.png" alt="Basket" className="w-[1300px] fixed top-[485px] items-center" />
+      <img
+        src="/images/tracker/basket.png"
+        alt="Basket"
+        className="fixed top-[485px] w-[1300px] items-center"
+      />
     </div>
   );
 }
