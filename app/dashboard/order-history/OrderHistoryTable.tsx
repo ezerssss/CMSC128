3eaceb9e72759a7d3ae479a6047835122d90e31f
Jullene Rae Arsenal <jsarsenal@up.edu.ph>
2024-11-26 00:21:45 +0800

    [Feature][Jullene] initial ui for profit tracker & order history design polish

--- app/dashboard/order-history/OrderHistoryTable.tsx
+++ app/dashboard/order-history/OrderHistoryTable.tsx
@@ -35,11 +35,11 @@ export default function OrderHistoryTable({ orders }: OrderHistoryTableProps) {
     <Table className="min-w-full table-auto overflow-hidden rounded-2xl">
       <TableHeader>
         <TableRow
-          className="rounded-t-2xl text-left text-sm font-semibold uppercase tracking-wide"
+          className="rounded-t-2xl text-left text-sm font-semibold uppercase tracking-wide whitespace-nowrap"
           style={{ backgroundColor: "#DBEAFF" }}
         >
           {tableHeaders.map((header) => (
-            <TableHead key={header} className="px-4 py-2 font-bold text-black">
+            <TableHead key={header} className="px-4 py-2 font-bold text-black text-[12px]">
               {header}
             </TableHead>
           ))}
@@ -50,23 +50,21 @@ export default function OrderHistoryTable({ orders }: OrderHistoryTableProps) {
         {orders.length > 0 ? (
           orders.map((order) => (
             <TableRow key={order.orderID} className="border-b border-gray-200">
-              <TableCell className="px-4 py-2">{order.orderID}</TableCell>
+              <TableCell className="px-2 py-2 text-[12px]">{order.orderID}</TableCell>
               <TableCell className="px-4 py-2">{order.name}</TableCell>
               <TableCell className="px-4 py-2">{order.address}</TableCell>
               <TableCell className="px-4 py-2">
-                {order.services.map((service) => (
-                  <p key={`${order.orderID}-${service}`}>{service}</p>
-                ))}
+                {order.services.join(', ')}
               </TableCell>
               <TableCell className="px-4 py-2">{order.bag}</TableCell>
-              <TableCell className="px-4 py-2">{order.weight}</TableCell>
+              <TableCell className="px-4 py-2 text-center">{order.weight}</TableCell>
               <TableCell className="px-4 py-2">{order.logistics}</TableCell>
               <TableCell className="px-4 py-2">
                 <Badge className={`${getBadgeColor(order.boardStatus)}`}>
                   {getBoardStatusTitle(order.boardStatus)}
                 </Badge>
               </TableCell>
-              <TableCell className="px-4 py-2">
+              <TableCell className="px-4 py-2 ">
                 <Badge className={`${getBadgeColor(order.paymentStatus)}`}>
                   {order.paymentStatus}
                 </Badge>
